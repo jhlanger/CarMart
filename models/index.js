5616a2a552b3a108008d5acf6bb4ff75207022ce
Jonhe <Jonhenrylangerman@gmail.com>
2021-09-14 21:01:21 -0700

    added model associations

---
+++ models/index.js
@@ -0,0 +1,31 @@
+// import all models
+const Cars = require('./Cars');
+const User = require('./User');
+const ProductTag = require('./ProductTag');
+const Tag = require('./Tag');
+
+// create associations
+//users can have many cars for sale
+User.hasMany(Cars, {
+    foreignKey: 'user_id'
+})
+// car can only have one seller
+Cars.belongsTo(User, {
+    foreignKey: 'user_id',
+    onDelete: 'SET NULL'
+})
+  // Cars belongToMany Tags (through ProductTag)
+  Cars.belongsToMany(Tag, {
+    through: ProductTag,
+    foreignKey: 'product_id'
+  });
+  
+  // Tags belongToMany Products (through ProductTag)
+  
+  Tag.belongsToMany(Cars, {
+    through: ProductTag,
+    foreignKey: 'tag_id'
+  });
+
+
+module.exports = { User, Cars, ProductTag, Tag };
