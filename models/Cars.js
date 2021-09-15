92438216c15085ca457a088a3ee3a296f3a50816
Jonhe <Jonhenrylangerman@gmail.com>
2021-09-14 20:55:04 -0700

    models

---
+++ models/Cars.js
@@ -0,0 +1,67 @@
+// import important parts of sequelize library
+const { Model, DataTypes, NUMERIC, DECIMAL } = require('sequelize');
+// import our database connection from config.js
+const sequelize = require('../config/connection');
+
+// Initialize Car model (table) by extending off Sequelize's Model class
+class Cars extends Model { }
+
+// set up fields and rules for Car model
+Cars.init(
+    {
+        // define columns
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        model: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        make: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        year: {
+            type: DataTypes.INTEGER,
+            allowNull: false
+        },
+        miles: {
+            type: DataTypes.INTEGER,
+            allowNull: false
+        },
+        price: {
+            type: DataTypes.DECIMAL(10, 2),
+            allowNull: false,
+            validate: {
+                isDecimal: true
+            }
+
+        },
+        user_id: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: 'user',
+                key: 'id'
+            }
+        },
+        tag_id: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: 'tag',
+                key: 'id'
+            }
+        }
+    },
+    {
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'cars',
+    }
+);
+
+module.exports = Cars;
