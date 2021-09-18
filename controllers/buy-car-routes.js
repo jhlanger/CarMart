780b3883e09d95025f99af6b9f3815d12ae8b1c7
jhlanger <jonhenrylangerman@gmail.com>
2021-09-17 23:08:20 -0700

    added :id routes

--- controllers/buy-car-routes.js
+++ controllers/buy-car-routes.js
@@ -30,6 +30,37 @@ router.get('/', (req, res) => {
     });
 });
 
+router.get('/:id', (req, res) => {
+    console.log('======================');
+    Cars.findOne({
+        where: {
+            id:req.params.id
+        },
+        include: [
+            {
+                model: User,
+                attributes: ['id', 'username', 'email']
+            },
+            {
+                model: Color,
+                attributes: ['id', 'color_name']
+            },
+            {
+                model: Tag,
+                attributes: ['id', 'tag_name']
+            }
+        ]
+    }).then(dbCarData => {
+        //const cars = dbCarData.map(car => car.get({ plain: true }));
+        //console.log(dbCarData[0]);
+        res.render('buyCars', dbCarData)
+       // res.json(dbCarData);
+    }).catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
 // test api data
 router.get('/api', (req, res) => {
     console.log('======================');
@@ -53,6 +84,37 @@ router.get('/api', (req, res) => {
         //console.log(dbCarData[0]);
         //res.render('buyCars', dbCarData)
         res.json(dbCarData);
+    }).catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
+router.get('/api/:id', (req, res) => {
+    console.log('======================');
+    Cars.findOne({
+        where: {
+            id:req.params.id
+        },
+        include: [
+            {
+                model: User,
+                attributes: ['id', 'username', 'email']
+            },
+            {
+                model: Color,
+                attributes: ['id', 'color_name']
+            },
+            {
+                model: Tag,
+                attributes: ['id', 'tag_name']
+            }
+        ]
+    }).then(dbCarData => {
+        //const cars = dbCarData.map(car => car.get({ plain: true }));
+        //console.log(dbCarData[0]);
+        //res.render('buyCars', dbCarData)
+       res.json(dbCarData);
     }).catch(err => {
         console.log(err);
         res.status(500).json(err);
