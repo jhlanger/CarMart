0ec7f14db71206d1dcd1676fcc25f9ffecd39c6a
jhlanger <jonhenrylangerman@gmail.com>
2021-09-19 01:50:42 -0700

    added working navbar

--- controllers/index.js
+++ controllers/index.js
@@ -3,10 +3,12 @@ const router = require('express').Router();
 //const apiRoutes = require('./api/');
 const buyCarRoutes = require('./buy-car-routes.js');
 const sellCarRoutes = require('./sell-car-routes.js');
+const homepageRoutes = require('./homepageRoutes.js');
 
 
 router.use('/buy', buyCarRoutes);
 router.use('/add', sellCarRoutes);
+router.use('/' , homepageRoutes);
 
 //router.use('/api', apiRoutes);
 
